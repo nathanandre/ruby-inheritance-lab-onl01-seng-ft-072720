8fe7140129136bb954dd887b269a32576fb98fd6
nathanandre <nathanandreburns@gmail.com>
2021-04-07 14:25:37 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,23 @@
-class Student < User
-
-    attr_reader :knowledge
-
-    def initialize
+class Student < User 
+    attr_accessor :first_name, :last_name 
+    def initialize 
+        @fist_name = first_name
+        @last_name = last_name 
         @knowledge = []
-    end
-
+    end 
+    def first_name 
+        @first_name
+    end 
+    def last_name 
+        @last_name
+    end 
+    def new 
+        @knowledge
+    end 
     def learn(string)
-        @knowledge << string
-    end
-
-end
+        @knowledge << string 
+    end 
+    def knowledge
+        new 
+    end 
+end 
